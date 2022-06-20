bdd3a5b6e8925964605628e58f39ce723d136c1f
sanjaybabu123 <sanjaybabu700@gmail.com>
2022-06-20 20:01:14 +0530

    added to new branch

--- com.crm.SDETcopy/src/test/java/com/crm/createContacts/CreateContactsTest.java
+++ com.crm.SDETcopy/src/test/java/com/crm/createContacts/CreateContactsTest.java
@@ -41,7 +41,7 @@ public class CreateContactsTest extends BaseClass{
 	@Test
 	public void createContactsTest() throws Throwable{
 
-	
+		//To generate random number 
 		int randNum = jLib.getRandomNumber();
 		
 		//Fetch first name from excelSheet
